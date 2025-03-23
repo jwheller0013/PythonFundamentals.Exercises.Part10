3f0ef123576eb54d42c5de5612127c6adce3ef02
jwheller0013 <jwheller0013@yahoo.com>
2025-03-23 17:09:08 -0400

    add_customer working

--- small_town_teller.py
+++ small_town_teller.py
@@ -14,3 +14,19 @@ class Account:
         self.balance = balance
 
 class Bank:
+    def __init__(self):
+        self.accounts: dict[int, Account] = dict()
+        self.customers: dict[int, Person] = dict()
+
+    def add_customer (self, customer: Person):
+        if customer.id in self.customers:
+            raise ValueError(f"Customer already exists with id {customer.id}.")
+        else:
+            self.customers[customer.id] = customer
+
+# test_bank = Bank()
+#
+# jim = Person(1, "jim", "jimson")
+# test_bank.add_customer(jim)
+# print(jim.id)
+# print(test_bank.customers)
