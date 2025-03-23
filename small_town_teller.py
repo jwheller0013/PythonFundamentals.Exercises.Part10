53f9e2a763759fb1fd3a27e80380360d8d71f712
jwheller0013 <jwheller0013@yahoo.com>
2025-03-23 16:46:52 -0400

    Starting classes

---
+++ small_town_teller.py
@@ -0,0 +1,16 @@
+
+
+class Person:
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+class Account:
+    def __init__(self, number, type, owner, balance):
+        self.number = number
+        self.type = type
+        self.owner = owner
+        self.balance = balance
+
+class Bank:
