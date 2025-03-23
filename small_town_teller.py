c38c0bb3c579e38f0f6dcfb4e38a7cf1f3fa62a8
jwheller0013 <jwheller0013@yahoo.com>
2025-03-23 17:51:11 -0400

    remove_account works

--- small_town_teller.py
+++ small_town_teller.py
@@ -2,16 +2,16 @@
 
 class Person:
     def __init__(self, id, first_name, last_name):
-        self.id = int(id)
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
 
 class Account:
-    def __init__(self, number, type, owner, balance):
-        self.number = int(number)
+    def __init__(self, number, type, owner):
+        self.number = number
         self.type = type
         self.owner = owner
-        self.balance = balance
+        self.balance = 0.0
 
 class Bank:
     def __init__(self):
@@ -32,16 +32,30 @@ class Bank:
         else:
             self.accounts[account.number] = account
 
+    def remove_account (self, account: Account):
+        if account.number not in self.accounts:
+            raise ValueError(f"{account.number} does not exist.")
+        else:
+            self.accounts = {y: x for y, x in self.accounts.items() if x != account}
+
 # test_bank = Bank()
 #
 # jim = Person(1, "jim", "jimson")
 # test_bank.add_customer(jim)
-# test_bank.add_customer(jim) #works for value error
+# # test_bank.add_customer(jim) #works for value error
 # print(jim.id)
 # print(test_bank.customers)
 #
-# jim_check = Account(1, "checking", jim, 0.0)
+# jim_check = Account(1, "checking", jim)
 # print(jim_check.number)
 # test_bank.add_account(jim_check)
-# test_bank.add_account(jim_check) #works for value error
+# # test_bank.add_account(jim_check) #works for value error
+# print(test_bank.accounts)
+#
+# bob = Person(2, "bob", "bobenstein")
+# test_bank.add_customer(bob)
+# bob_checking = Account(2, "checking", bob)
+# test_bank.add_account(bob_checking)
+# print(test_bank.accounts)
+# test_bank.remove_account(bob_checking)
 # print(test_bank.accounts)
